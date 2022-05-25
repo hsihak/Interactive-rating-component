261340144ce39c751d99451f9b143b9ff137b9e6
hsihak <sihak.hs@gmail.com>
2022-05-25 19:38:05 -0400

    Desktop Version with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // =>
+      }, 
+      colors: {
+        'primary': 'hsl(25, 97%, 53%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': '#1f252e',
+        'dark-blue-circle': '#29313d',
+        'very-dark-blue': 'hsl(216, 12%, 8%)'
+      },
+      padding: {
+        '1rem': '1rem'
+      },
+    }
+  },
+  plugins: [],
+}
